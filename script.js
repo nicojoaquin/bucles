bde4ce98dad522f82838e8b4833e14a57aa0e063
nicojoaquin <nicojoaquin1998@gmail.com>
2022-02-02 20:08:05 -0300

    Terminado

--- script.js
+++ script.js
@@ -100,8 +100,8 @@ const getRandomNum = (min, max) => {
 
 // console.log(".................");
 
-//Ejercicio 9
-let par = "Lorem ipsum dolor sit amet consectetur adipiscing elit Curabitur eget bibendum turpis Curabitur scelerisque eros ultricies venenatis mi at tempor nisl Integer tincidunt accumsancursus";
+// //Ejercicio 9
+// let par = "Lorem ipsum dolor sit amet consectetur adipiscing elit Curabitur eget bibendum turpis Curabitur scelerisque eros ultricies venenatis mi at tempor nisl Integer tincidunt accumsancursus";
 
 // const contarLetra = (letra) => {
 //   let cantidad = 0;
@@ -149,3 +149,55 @@ let par = "Lorem ipsum dolor sit amet consectetur adipiscing elit Curabitur eget
 // console.log(invertido);
 
 // console.log(".................");
+
+//Ejercicio 11
+// let mix = [42, true, "towel", [2,1], 'hello', 34.4, {"name": "juan"}];
+// for (let i = 0; i < mix.length; i++) {
+//   console.log(mix[i], typeof(mix[i])); 
+// }
+
+// console.log(".................");
+
+//Ejercicio 12
+// let myArray12 =[
+//   3344,34334,454543,342534,4563456,3445,23455,234,262,2335,43323,
+//   4356,345,4545,452,345,434,36,345,4334,5454,345,4352,23,365,345,
+//   47,63,425,6578759,768,834,754,35,32,445,453456,56,7536867,3884526,
+//   4234,35353245,53244523,566785,7547,743,4324,523472634,26665,63432,
+//   54645,32,453625,7568,5669576,754,64356,542644,35,243,371,3251,351223,
+//   13231243,734,856,56,53,234342,56,545343
+// ];
+
+// myArray12.forEach( (num, i) => {
+ 
+//   if( num % 14 === 0) {
+//     console.log(num);
+//   }
+ 
+// });
+
+// console.log(".................");
+
+//Ejercicio 13
+// let i = 20;
+// do {
+//   if( (i % 5) === 0 ) {
+//     console.log(i + "!");
+//   }
+//   i--;
+// } while (i > 0);
+
+
+// console.log(".................");
+
+//Ejercicio 14
+// let myArray14 = [43,23,6,87,43,1,4,6,3,67,8,3445,3,7,5435,63,346,3,456,734,6,34];
+
+// let max = 0;
+
+// for ( let numero of myArray14 ) {
+//   if (max < numero)
+//     max = numero;
+// }
+
+// console.log(max);
